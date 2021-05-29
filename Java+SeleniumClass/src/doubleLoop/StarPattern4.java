885b97c5dbf127e465f2be7b6538d3811db94278
Lenovo Ideapad 330s <Lenovo Ideapad 330s@LAPTOP-6RNAMR5E>
2021-05-29 19:09:46 +0530

    Double loop programs

---
+++ Java+SeleniumClass/src/doubleLoop/StarPattern4.java
@@ -0,0 +1,20 @@
+package doubleLoop;
+public class StarPattern4
+{
+	public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 4; j >= i; j--)
+            {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++)
+            {
+            System.out.print("*");
+            }
+            System.out.println();
+        }
+	}
+}
+	
+	
